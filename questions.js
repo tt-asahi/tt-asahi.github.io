1f4e252f3b6a4601f25d3a26244f7f36e0f431d9
tt-asahi <67936722+tt-asahi@users.noreply.github.com>
2022-09-28 13:59:58 +0900

    41まで

--- questions.js
+++ questions.js
@@ -1,4 +1,29 @@
-const weight = 58;
-const height = 1.65;
+const answersList = document.querySelectorAll('ol.answers li');
 
-const bmi = weight / (height ** 2);
+answersList.forEach(li => li.addEventListener('click', checkClickedAnswer));
+
+function checkClickedAnswer(event){
+    const checkClickedAnswerElement = event.currentTarget;
+    let message;
+    let answerColorCode;
+
+    const selectedAnswer = checkClickedAnswerElement.dataset.answer;
+    const correctAnswer = 'B';
+
+    if(selectedAnswer === correctAnswer){
+        message = '正解です';
+        answerColorCode = '';
+
+    }else{
+        message = '不正解です';
+        answerColorCode = '#a3057e';
+
+    }
+
+    
+
+    alert(message);
+    document.querySelector('span#correct-answer').style.color = answerColorCode;
+    document.querySelector('#section-correct-answer').style.display = 'block';
+    
+}
