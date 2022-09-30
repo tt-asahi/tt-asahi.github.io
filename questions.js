8ce97589a8cc144f970a219a5a3982e9592bf56e
tt-asahi <67936722+tt-asahi@users.noreply.github.com>
2022-09-30 15:32:02 +0900

    完成

--- questions.js
+++ questions.js
@@ -2,28 +2,33 @@ const answersList = document.querySelectorAll('ol.answers li');
 
 answersList.forEach(li => li.addEventListener('click', checkClickedAnswer));
 
+const correctAnswers = {
+    question1: 'B',
+    question2: 'D',
+    question3: 'C',
+    question4: 'A',
+    question5: 'D',
+}
+
 function checkClickedAnswer(event){
-    const checkClickedAnswerElement = event.currentTarget;
+    const clickedAnswerElement = event.currentTarget;
     let message;
     let answerColorCode;
 
-    const selectedAnswer = checkClickedAnswerElement.dataset.answer;
-    const correctAnswer = 'B';
+    const selectedAnswer = clickedAnswerElement.dataset.answer;
+    const questionId = clickedAnswerElement.closest('ol.answers').dataset.id;
+    const correctAnswer = correctAnswers[questionId];
 
     if(selectedAnswer === correctAnswer){
-        message = '正解です';
+        message = '正解です!!!';
         answerColorCode = '';
-
+        document.querySelector('#section-correct-answer').style.display = 'block';
     }else{
-        message = '不正解です';
+        message = '不正解です...';
         answerColorCode = '#a3057e';
-
     }
-
-    
 
     alert(message);
     document.querySelector('span#correct-answer').style.color = answerColorCode;
-    document.querySelector('#section-correct-answer').style.display = 'block';
-    
+       
 }
